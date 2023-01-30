88318687fc934845971967950cbfaf945cc375da
Franciszek Sikora <franciszeksikora@MacBook-Pro-Franciszek.local>
2023-01-30 12:16:44 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import ReactPlayer from 'react-player'
+
+function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <ReactPlayer
+        url={'https://vps.blady.dev/live/dog/index.mpd'}
+        controls={true}
+      />
+    </div>
+  )
+}
+
+export default App
