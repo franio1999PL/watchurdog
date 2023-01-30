a3cb3fee05753d5b4baf996e7c614d09ce391de9
Franciszek Sikora <franciszeksikora@MacBook-Pro-Franciszek.local>
2023-01-30 12:26:25 +0100

    third

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import ReactPlayer from 'react-player'
+import ReactHlsPlayer from 'react-hls-player'
 
 function App () {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <ReactPlayer
+      {/* <ReactPlayer
         url={'https://vps.blady.dev/live/dog/index.m3u8'}
         controls={true}
+      /> */}
+
+      <ReactHlsPlayer
+        src='https://vps.blady.dev/live/dog/index.m3u8'
+        autoPlay={true}
+        controls={true}
+        width='100%'
+        height='auto'
       />
     </div>
   )
